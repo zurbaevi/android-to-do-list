75d0efbf116ed13c2dca2f3ac6d21ba7ce2fe75a
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-30 03:19:48 +0400

    Updated MainActivity.kt

--- app/src/main/java/dev/zurbaevi/todolist/ui/MainActivity.kt
+++ app/src/main/java/dev/zurbaevi/todolist/ui/MainActivity.kt
@@ -2,12 +2,15 @@ package dev.zurbaevi.todolist.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dev.zurbaevi.todolist.R
+import dev.zurbaevi.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
