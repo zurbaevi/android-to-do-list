9d8555078ca5ecac1fb2042101f76b0c29bba445
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-09-14 22:17:27 +0400

    Minor improvements

--- app/src/main/java/dev/zurbaevi/todolist/service/Constants.kt
+++ app/src/main/java/dev/zurbaevi/todolist/service/Constants.kt
@@ -2,7 +2,8 @@ package dev.zurbaevi.todolist.service
 
 object Constants {
 
-    const val DATABASE_NAME = "task_table"
+    const val DATABASE_NAME = "task_database"
+    const val DATABASE_TABLE_NAME = "task_table"
     const val DATABASE_VERSION = 2
 
 }
