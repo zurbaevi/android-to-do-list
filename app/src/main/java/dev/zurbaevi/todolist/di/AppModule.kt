a5523f64ac823530fec1925ece7bdd2079d75fb0
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2022-01-28 16:17:23 +0400

    Update AppModule.kt

--- app/src/main/java/dev/zurbaevi/todolist/di/AppModule.kt
+++ app/src/main/java/dev/zurbaevi/todolist/di/AppModule.kt
@@ -1,18 +1,15 @@
 package dev.zurbaevi.todolist.di
 
-import android.content.Context
 import androidx.room.Room
 import dev.zurbaevi.todolist.data.Constants
 import dev.zurbaevi.todolist.data.local.TaskDatabase
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
-    single { provideTaskDatabase(get()) }
-    single { provideTaskDao(get()) }
+    single {
+        Room.databaseBuilder(
+            androidContext(), TaskDatabase::class.java, Constants.DATABASE_NAME
+        ).build().taskDao()
+    }
 }
-
-private fun provideTaskDatabase(context: Context) = Room.databaseBuilder(
-    context, TaskDatabase::class.java, Constants.DATABASE_NAME
-).build()
-
-private fun provideTaskDao(database: TaskDatabase) = database.taskDao()
